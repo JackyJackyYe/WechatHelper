c44300217cb830eb15b4459405ff25ff2c756784
JackyYe <Jack--Ye@outlook.com>
2023-11-17 20:54:59 +0800

    初始版本框架提交

---
+++ wechat-helper-accessibility/src/main/java/site/jackye/wechat_helper_accessibility/Wrapper.kt
@@ -0,0 +1,40 @@
+package site.jackye.wechat_helper_accessibility
+
+import android.graphics.Rect
+import android.view.accessibility.AccessibilityNodeInfo
+
+/**
+ * Author: JackyYe
+ * Date: 2023-11-15
+ * Desc: 实体包装类
+ */
+data class EventWrapper(
+    var packageName: String,
+    var className: String,
+    var eventType: Int
+) {
+    override fun toString() = "$packageName → $className → $eventType"
+}
+
+
+/**
+ * 视图结点包装类
+ * */
+data class NodeWrapper(
+    var text: String? = null,
+    var id: String? = null,
+    var bounds: Rect? = null,
+    var className: String,
+    var description: String? = null,
+    var clickable: Boolean = false,
+    var scrollable: Boolean = false,
+    var editable: Boolean = false,
+    var nodeInfo: AccessibilityNodeInfo? = null
+) {
+    override fun toString() = "$className → $text → $id → $description → $bounds → $clickable → $scrollable → $editable"
+}
+
+
+data class AnalyzeSourceResult(
+    val nodes: ArrayList<NodeWrapper> = arrayListOf()
+)
